cf27c334a1cb7b81fbfabd0f5d916b79893f525c
r-toyama <tagjmp@gmail.com>
2025-08-23 02:25:19 +0000

    change

--- terraform/agent.tf
+++ terraform/agent.tf
@@ -75,6 +75,8 @@ resource "aws_ecr_repository" "bedrock_agent_repo" {
   name                 = "bedrock-agent"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
